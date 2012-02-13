51ebfa3a1a58ec732790b0ef24e17ba0e912eff0
Jake Luer <jake.luer@gmail.com>
2012-02-13 13:53:29 -0500

    more comments

--- support/compile.js
+++ support/compile.js
@@ -1,10 +1,31 @@
+/*!
+ * chai-spies :: browser build script
+ * Copyright (c) 2012 Jake Luer <jake@alogicalparadox.com>
+ * MIT Licensed
+ */
+
+/*!
+ * Script dependancies
+ */
+
 var fs = require('fs')
   , path = require('path')
   , join = path.join
   , folio = require('folio');
 
+/*!
+ * Script variables
+ */
+
 var appfiles = []
   , basepath = join(__dirname, '..', 'lib');
+
+/**
+ * Recursively iterate through a given path
+ * and add all `.js` files to an array.
+ *
+ * @param {String} absolute path
+ */
 
 function iteratePath (p) {
   var self = this
@@ -21,7 +42,17 @@ function iteratePath (p) {
   });
 };
 
+// Go!
 iteratePath(basepath);
+
+/**
+ * Package together all found files into a
+ * folio.Glossary, defining a custom "compiler"
+ * that will wrap our script with the needed commonjs
+ *
+ * @param {Array} files
+ * @param {Object} folio glossary configuration
+ */
 
 var applicationJs = new folio.Glossary(appfiles, {
   compilers: {
@@ -35,8 +66,18 @@ var applicationJs = new folio.Glossary(appfiles, {
   }
 });
 
+/*!
+ * Load up our prefix/suffix
+ */
+
 var prefix = fs.readFileSync(join(__dirname, 'browser', 'prefix.js'), 'utf8')
   , suffix = fs.readFileSync(join(__dirname, 'browser', 'suffix.js'), 'utf8')
+
+/**
+ * Compile the folio.Glossary, applying our wrapper,
+ * and output the src. Wrap with prefix/suffix and
+ * write to file.
+ */
 
 applicationJs.compile(function (err, src) {
   var content = prefix + src + suffix;
