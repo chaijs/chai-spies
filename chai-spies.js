060141c46e29b0b3f14330d03e0af77faac87004
Jake Luer <jake.luer@gmail.com>
2012-07-09 12:02:56 -0400

    one more try

--- chai-spies.js
+++ chai-spies.js
@@ -2,13 +2,10 @@
   if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
     module.exports = definition(module, exports);
   } else {
-    var mod = {};
-    mod.exports = {};
+    var mod = { exports: {} };
     definition.call(mod.exports, mod, mod.exports);
     if (typeof define === 'function' && typeof define.amd  === 'object') {
-      define(function () {
-        return mod.exports;
-      });
+      define(function () { return mod.exports; });
     } else {
       if (!context.chai) throw new Error('Chai cannot be found in current scope.');
       context.chai.use(mod.exports);
