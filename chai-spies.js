a2bf4b9ac2fec3b5d5a5b8f3251aa47ba405b9b8
Jake Luer <jake.luer@gmail.com>
2012-07-09 12:01:04 -0400

    improve amd wrapper

--- chai-spies.js
+++ chai-spies.js
@@ -1,14 +1,18 @@
 !function (context, definition) {
   if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
     module.exports = definition(module, exports);
-  } else if (typeof define === 'function' && typeof define.amd  === 'object') {
-    define(definition);
   } else {
-    if (!context.chai) throw new Error('Chai cannot be found in current scope.');
     var mod = {};
     mod.exports = {};
     definition.call(mod.exports, mod, mod.exports);
-    context.chai.use(mod.exports);
+    if (typeof define === 'function' && typeof define.amd  === 'object') {
+      define(function () {
+        return mod.exports;
+      });
+    } else {
+      if (!context.chai) throw new Error('Chai cannot be found in current scope.');
+      context.chai.use(mod.exports);
+    }
   }
 }(this, function (module, exports) {
 
